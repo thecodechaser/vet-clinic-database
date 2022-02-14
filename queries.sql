577b0de5249eeed5361a945f3c76acce98384e07
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-02-14 17:08:39 +0530

    Update README file

--- queries.sql
+++ queries.sql
@@ -1,9 +1,9 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name like '%mon%';
+SELECT * from animals WHERE name LIKE '%mon';
 SELECT name from animals WHERE date_of_birth BETWEEN Jan 1, 2016 AND Dec 31, 2019;
 SELECT name from animals WHERE (neutered = 0 && escape_attempts < 3);
-SELECT date_of_birth from animals WHERE name = 'Agumon' OR name = 'Pikachu';
+SELECT date_of_birth from animals WHERE (name = 'Agumon' OR name = 'Pikachu');
 SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
 SELECT * from animals WHERE neutered = 0;
 SELECT * from animals WHERE name != 'Gabumon';
