3834f242232a914843c222dbdcf9ff49ba24f27c
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-02-14 16:33:22 +0530

    Insert data

--- data.sql
+++ data.sql
@@ -1,6 +1,6 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals VALUES (ID, name, date_of_birth, escape_attempts, neutered, weight_kg) (1, 'Agumon', Feb 3rd, 2020, 0, 0, 10.23);
-INSERT INTO animals VALUES (ID, name, date_of_birth, escape_attempts, neutered, weight_kg) (2, 'Gabumon', Nov 15th, 2018, 0, 0, 10.23);
-INSERT INTO animals VALUES (ID, name, date_of_birth, escape_attempts, neutered, weight_kg) (3, 'Pikachu', Jan 7th, 2021, 0, 0, 10.23);
-INSERT INTO animals VALUES (ID, name, date_of_birth, escape_attempts, neutered, weight_kg) (4, 'Devimon',  May 12th, 2017, 0, 0, 10.23);
+INSERT INTO animals VALUES (ID, name, date_of_birth, escape_attempts, neutered, weight_kg) (2, 'Gabumon', Nov 15th, 2018, 2, 0, 8);
+INSERT INTO animals VALUES (ID, name, date_of_birth, escape_attempts, neutered, weight_kg) (3, 'Pikachu', Jan 7th, 2021, 1, 1, 15.04);
+INSERT INTO animals VALUES (ID, name, date_of_birth, escape_attempts, neutered, weight_kg) (4, 'Devimon',  May 12th, 2017, 5, 0, 11);
