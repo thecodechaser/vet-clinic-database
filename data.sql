6117e62b636c61ccf709769051855182b7ff69b6
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-02-14 16:31:48 +0530

    Create table

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals VALUES (ID, name, date_of_birth, escape_attempts, neutered, weight_kg) (1, 'Agumon', Feb 3rd, 2020, 0, 0, 10.23);
+INSERT INTO animals VALUES (ID, name, date_of_birth, escape_attempts, neutered, weight_kg) (2, 'Gabumon', Nov 15th, 2018, 0, 0, 10.23);
+INSERT INTO animals VALUES (ID, name, date_of_birth, escape_attempts, neutered, weight_kg) (3, 'Pikachu', Jan 7th, 2021, 0, 0, 10.23);
+INSERT INTO animals VALUES (ID, name, date_of_birth, escape_attempts, neutered, weight_kg) (4, 'Devimon',  May 12th, 2017, 0, 0, 10.23);
