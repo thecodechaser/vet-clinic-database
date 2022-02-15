de2e061906c3948bc84bb572cc0d2cf4678cc30c
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-02-15 15:25:07 +0530

    Add queries

--- queries.sql
+++ queries.sql
@@ -8,3 +8,38 @@ SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
 SELECT * from animals WHERE neutered = 0;
 SELECT * from animals WHERE name != 'Gabumon';
 SELECT * from animals WHERE (weight_kg >= 10.4 && weight_kg <= 17.3);
+
+BEGIN;
+UPDATE animals SET species ='unspecified';
+ROLLBACK;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals
+ROLLBACK;
+
+SELECT name from animals;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > Jan 1st, 2022;
+SAVEPOINT savepoint1;
+UPDATE animals SET weight_kg = weight_kg*-1;
+ROLLBACK TO savepoint1;
+UPDATE animals SET weight_kg = weight_kg*-1 WHERE weight_kg < 0;
+COMMIT;
+
+SELECT COUNT(name) FROM animals;
+SELECT COUNT(name) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT MAX(escape_attempts) FROM animals;
+SELECT COUNT(neutered) FROM animals WHERE neutered = 1;
+SELECT COUNT(neutered) from animals WHERE neutered = 0;
+SELECT MIN(species) FROM animals GROUP BY species;
+SELECT MAX(species) FROM animals GROUP BY species;
+SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN Jan 1, 1990 AND Dec 31, 2000 GROUP BY species;
+
+
